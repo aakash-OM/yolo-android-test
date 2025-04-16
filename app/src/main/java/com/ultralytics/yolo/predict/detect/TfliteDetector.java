abc07715776663d64e2d5906d486fa8e508051a0
Aakash <aakashmittal0207@gmail.com>
2025-04-16 12:56:18 +0530

    TfliteDetector.java file updated

--- app/src/main/java/com/ultralytics/yolo/predict/detect/TfliteDetector.java
+++ app/src/main/java/com/ultralytics/yolo/predict/detect/TfliteDetector.java
@@ -2,19 +2,34 @@ package com.ultralytics.yolo.predict.detect;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.RectF;
+import android.content.res.AssetFileDescriptor;
+
 import org.tensorflow.lite.Interpreter;
+
 import com.ultralytics.yolo.ImageProcessing;
+import com.ultralytics.yolo.models.LocalYoloModel;
+import com.ultralytics.yolo.models.YoloModel;
 import com.ultralytics.yolo.predict.Predictor;
+import com.ultralytics.yolo.predict.DetectedObject;
+
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 public class TfliteDetector extends Predictor {
-    private Interpreter tflite;
-    private List<DetectedObject> detections;
+    private Interpreter interpreter;
+    private Object[] inputArray;
+    private Map<Integer, Object> outputMap;
+    private List<String> labels;
 
     public TfliteDetector(Context context) {
         super(context);
@@ -22,31 +37,61 @@ public class TfliteDetector extends Predictor {
 
     @Override
     public void loadModel(YoloModel yoloModel, boolean useGpu) throws Exception {
-        // Load TFLite model from assets
-        tflite = new Interpreter(loadModelFile(context, yoloModel.modelPath));
-        loadLabels(context.getAssets(), yoloModel.metadataPath); // Load class labels
+        if (yoloModel instanceof LocalYoloModel) {
+            LocalYoloModel localModel = (LocalYoloModel) yoloModel;
+            labels = loadLabels(context.getAssets(), localModel.metadataPath);
+            interpreter = new Interpreter(loadModelFile(context, localModel.modelPath));
+        }
     }
 
     @Override
     public JSONArray predict(Bitmap bitmap) throws JSONException {
-        // Preprocess image (resize + normalize)
         ByteBuffer inputBuffer = ImageProcessing.bitmapToInputBuffer(bitmap);
-        // Run inference
-        float[][][] output = new float[1][8400][7]; // Shape depends on your model
-        tflite.run(inputBuffer, output);
-        // Postprocess to JSON
-        detections = parseOutput(output[0]);
-        return convertDetectionsToJSON(detections);
+        inputArray = new Object[]{inputBuffer};
+        outputMap = new HashMap<>();
+        ByteBuffer outputBuffer = ByteBuffer.allocateDirect(8400 * 7 * 4);
+        outputBuffer.order(java.nio.ByteOrder.nativeOrder());
+        outputMap.put(0, outputBuffer);
+
+        interpreter.runForMultipleInputsOutputs(inputArray, outputMap);
+        ArrayList<DetectedObject> detections = parseOutput(outputBuffer);
+        return convertToJSON(detections);
     }
 
-    private List<DetectedObject> parseOutput(float[][] output) {
-        List<DetectedObject> results = new ArrayList<>();
-        for (float[] det : output) {
-            if (det[4] > 0.5) { // Confidence threshold
-                RectF box = new RectF(det[0], det[1], det[0] + det[2], det[1] + det[3]);
-                results.add(new DetectedObject(det[4], box, (int) det[5], labels.get((int) det[5])));
+    private ArrayList<DetectedObject> parseOutput(ByteBuffer outputBuffer) {
+        ArrayList<DetectedObject> results = new ArrayList<>();
+        outputBuffer.rewind();
+        for (int i = 0; i < 8400; i++) {
+            float x = outputBuffer.getFloat();   // Center X
+            float y = outputBuffer.getFloat();   // Center Y
+            float w = outputBuffer.getFloat();   // Width
+            float h = outputBuffer.getFloat();   // Height
+            float conf = outputBuffer.getFloat(); // Confidence
+            int classId = outputBuffer.getInt();  // Class ID
+
+            if (conf > 0.5f && classId >= 0 && classId < labels.size()) {
+                RectF box = new RectF(x - w / 2, y - h / 2, x + w / 2, y + h / 2);
+                results.add(new DetectedObject(conf, box, classId, labels.get(classId)));
             }
         }
         return results;
+    }
+
+    private JSONArray convertToJSON(ArrayList<DetectedObject> detections) throws JSONException {
+        JSONArray jsonArray = new JSONArray();
+        for (DetectedObject obj : detections) {
+            jsonArray.put(obj.toJSON());
+        }
+        return jsonArray;
+    }
+
+    private ByteBuffer loadModelFile(Context context, String modelPath) throws IOException {
+        AssetFileDescriptor fileDescriptor = context.getAssets().openFd(modelPath);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
     }
 }
